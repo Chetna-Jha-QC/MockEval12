1e61bcd3ac9ef29a4f0f497d90c5bd075a385a16
Chetna-Jha-QC <ms.chetnajha@gmail.com>
2024-12-26 17:19:31 +0530

    front

---
+++ script/questions.js
@@ -0,0 +1,95 @@
+const apiBaseUrl = "https://florentine-tortoiseshell-condorraptor.glitch.me/users";//deployed link
+ 
+document.getElementById("addQusetionForm").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const questionData ={
+        statement: document.getElementById("addquestionForm").addEventListener("submit", async (e) => {
+
+        })
+    }
+})
+try{
+        alert("Question Created");
+        fetchAndRenderQuestions();
+ } catch (error) {
+        console.error("Error adding question:", error);
+    }
+
+// fetch and display the questions
+async function fetchAndRenderQuestions() {
+    try{
+        const response = await fetch(`${apiBaseUrl}/questions`);
+        const question = await response.json();
+
+        questionsContainer.innerHTML = ""; //clear the container
+
+        question.forEach((question) => {
+            const questionCard = document.createElement("div");
+            questionCard.className = "question-card";
+            questionCard.style.borderColor = question.reviewStatus ? "voilet" : "blue";
+
+            questionCard.innerHTML =`
+            <p><strong>Q:</strong> ${question.statement}</p>
+            <ul>
+             <li>A: ${question.options.A}</li>
+             <li>B: ${question.options.B}</li>
+             <li>C: ${question.options.C}</li>
+             <li>D: ${question.options.D}</li>
+             </ul>
+             <button class ="review-btn" data-id="${question.id}">Review</button>
+             <button class ="delete-btn" data-id="${question.id}">Delete</button>
+             `;
+
+            questionsContainer.appendChild(questionCard);
+        });
+
+        addEventListener();
+    } catch (error){
+        console.error("Error fetching questions:", error);
+    }
+}
+
+//adding event listner to btns
+
+function addEventListener(){
+    document.querySelectorAll(".review-btn").forEach((button) =>
+    button.addEventListener("click", async (e) => {
+        const questionID = e.target.getAtteibute("data-id");
+
+        if(confirm("Are you sure to review?")) {
+            try {
+                await fetch(`${apiBaseUrl}/questions/${questionID}`, {
+                    method: "PATCH",
+                    headers: {"Content-Type":"application/json"},
+                    body: JSON.stringify({reviewStatus: true}),
+                });
+                fetchAndRenderQuestions();
+            } catch (error) {
+                console.error("Error",error);
+            }
+        }
+    })
+);
+
+document.querySelectorAll(".delete-btn").forEach((button) =>
+    button.addEventListener("click", async (e) => {
+        const questionID = e.target.getAtteibute("data-id");
+
+        if(confirm("Are you sure to delete?")) {
+            try {
+                await fetch(`${apiBaseUrl}/questions/${questionID}`, {
+                    method: "DELETE",
+                });
+                fetchAndRenderQuestions();
+            } catch (error) {
+                console.error("Error in deleteing",error);
+            }
+        }
+    })
+);
+
+}
+
+// initial fetch and render
+fetchAndRenderQuestions();
